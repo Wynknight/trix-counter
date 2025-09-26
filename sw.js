7f256eb6e24ab8501ac6419c28797accb2a805e7
Wynknight <ybasel286@gmail.com>
2025-09-26 21:30:22 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,37 +1,5 @@
-
-const CACHE_NAME = 'trix-pwa-v4';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.webmanifest',
-  './icons/icon-192.png',
-  './icons/icon-512.png'
-];
-
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(k => {
-      if (k !== CACHE_NAME) return caches.delete(k);
-    })))
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', (e) => {
-  const req = e.request;
-  e.respondWith(
-    caches.match(req).then(cached => cached || fetch(req).then(res => {
-      // Optionally cache new requests (only GET)
-      if (req.method === 'GET' && res && res.status === 200 && res.type === 'basic') {
-        const clone = res.clone();
-        caches.open(CACHE_NAME).then(c => c.put(req, clone));
-      }
-      return res;
-    }).catch(() => caches.match('./')))
-  );
-});
+const CACHE_NAME='trix-pwa-v5';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>{if(k!==CACHE_NAME)return caches.delete(k);})))));self.clients.claim();});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{if(e.request.method==='GET'&&res&&res.status===200){const clone=res.clone();caches.open(CACHE_NAME).then(c=>c.put(e.request,clone));}return res;}).catch(()=>caches.match('./'))));});
